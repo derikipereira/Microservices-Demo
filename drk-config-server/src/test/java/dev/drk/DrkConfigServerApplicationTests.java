b6c1b156e7aab0f582da21d3f69274a5a8a490ef
Deriki Pereira <derikipereira@gmail.com>
2023-11-07 06:06:26 -0300

    up

---
+++ drk-config-server/src/test/java/dev/drk/DrkConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrkConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
